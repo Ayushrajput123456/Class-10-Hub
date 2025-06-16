a39808faba19187afcc92bfb32d8d769112de5ba
Ayush singh <kingayush239@gmail.com>
2025-06-16 10:48:43 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("welcome to class 10 hub")
+st.header("Class 10 Hub-your All in one NCERT companion")
+st.text("Introduction ") 
+st.text("Welcome to Class 10 Hub,a websit designed exclusively for class 10 students.") 
+st.text('''Here you can find:All NCERT pdf subject like- Maths, English, Hindi, Sanskrit, Science, SST. 
+Short notes to revise chapter quickly.''')
